e22ae25b3ea4dff7e56da5f4664a029e59c60c3d
mmmatthew <matthew.mdv@gmail.com>
2016-06-10 14:20:27 +0200

    added file formats and libraries to description

--- code/1_project_boundary_3d.py
+++ code/1_project_boundary_3d.py
@@ -3,9 +3,15 @@ This task projects 2D boundary into 3D with the elevation model, in order to the
 
 reads:
  - 2D boundary multipolygon
- - raster elevation file
+    - format: ESRI shapefile,
+    - library for reading, storing, editing: OGR
+ - raster elevation
+    - format: geotiff
+    - library: GDAL
 writes:
  - 3D boundary multipolygon
+    - format: GeoJSON
+    - location: project home directory/1_project_boundary_3d
 tasks:
  - Loop through vertices of boundary multipolygon and for each, fetch value of DEM and write to a 3D boundary multipolygon.
 """

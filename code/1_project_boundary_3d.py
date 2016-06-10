201b0ee69c6ad3dee73bbc6e374fe71002b862e1
mmmatthew <matthew.mdv@gmail.com>
2016-06-10 14:55:08 +0200

    added file formats and libraries to description

--- code/1_project_boundary_3d.py
+++ code/1_project_boundary_3d.py
@@ -3,15 +3,17 @@ This task projects 2D boundary into 3D with the elevation model, in order to the
 
 reads:
  - 2D boundary multipolygon
-    - format: ESRI shapefile,
+    - location: defined in settings file
+    - format: ESRI shapefile
     - library for reading, storing, editing: OGR
  - raster elevation
+    - location: defined in settings file
     - format: geotiff
     - library: GDAL
 writes:
  - 3D boundary multipolygon
     - format: GeoJSON
-    - location: project home directory/1_project_boundary_3d
+    - location: [project home directory]/1_project_boundary_3d/boundary3D.json
 tasks:
  - Loop through vertices of boundary multipolygon and for each, fetch value of DEM and write to a 3D boundary multipolygon.
 """
--- code/2_project_boundary_2d.py
+++ code/2_project_boundary_2d.py
@@ -2,16 +2,27 @@
 
 Reads:
  - Camera calibration parameter file
+    - location: defined by user in settings file
+    - Pix4D name: [prject name]_calibrated_camera_parameters.txt
+    - read with: the calibrated camera parameter matrices are saved as numpy arrays in a dictionary
  - offset for calibration parameters
- - 3D boundary file
+    - location: defined by user in settings file
+    - Pix4D name: [project name]_offset.xyz
+    - read with: saved as a list
+ - 3D boundary file:
+    - location: see output from previous step
+    - format: see output from previous step
+    - library: OGR
 Tasks:
- - interpret calibration parameters
+ - read calibration parameters to dictionary
  - For each image (camera), project 3D boundary into 2D with help of the
    calibration parameters (pmatrix with offset). This is done by looping through vertices and transforming each one.
+ - WARNING: because the transformation function is only valid around the center of focus of the camera, we have to delete some of the vertices.
 Writes:
  - N 2D boundary multipolygons, where N is the number of cameras
+    - format: GeoJSON
+    - location: [project home directory]/2_project_boundary_2d/[image name]_boundary2D.json
 Tools:
  - OGR to read and write polygons
- - GDAL ReadAsArray() or readRaster()
 
 """

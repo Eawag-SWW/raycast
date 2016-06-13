8240a3a5bbbbe7f989d12399efec6b062743811c
mmmatthew <matthew.mdv@gmail.com>
2016-06-13 09:11:16 +0200

    added file formats and libraries to description

--- code/3_clip_images_2d.py
+++ code/3_clip_images_2d.py
@@ -3,11 +3,19 @@ Clip each image with its corresponding projected boundary file
 
 Reads:
  - 2D boundary multipolygon file for each camera
- - image for each camera
-Writes:
- - N clipped images, where N is the number of cameras
+    - location: see output from previous step
+    - format: see output from previous step
+    - library: OGR
+ - undistorted image for each camera
+    - location: defined by user in settings file
+    - format: TIFF image
+    - library: GDAL
 Tasks:
  - clip each image using the corresponding multipolygon
 Tools:
  - rasterclipper.py
+Writes:
+ - N clipped images, where N is the number of cameras
+    - location: [project home directory]/3_clip_images_2d/[image name]_clipped.tif
+    - format: TIFF image
 """
